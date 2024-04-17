d837260c7ffd84670746384416d41d1ca6203548
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-17 11:23:08 +0200

    added code to program.cs needed to run the program

---
+++ Howest.MagicCards.WebAPI/Controllers/CardsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Howest.MagicCards.WebAPI.Controllers
+{
+    public class CardsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Howest.MagicCards.WebAPI/extensions/WebApplicationExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Howest.MagicCards.WebAPI.extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void Deconstruct(this WebApplicationBuilder builder,
+            out WebApplicationBuilder applicationBuilder,
+            out IServiceCollection services,
+            out ConfigurationManager configuration)
+        {
+            applicationBuilder = builder;
+            services = builder.Services;
+            configuration = builder.Configuration;
+        }
+    }
+}

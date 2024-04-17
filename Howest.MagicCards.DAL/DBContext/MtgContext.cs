a841905cf952ed38ce2b4d860735e9e8521dc6ef
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-17 10:55:04 +0200

    started with CardRepository

--- Howest.MagicCards.DAL/DBContext/MtgContext.cs
+++ Howest.MagicCards.DAL/DBContext/MtgContext.cs
@@ -36,9 +36,9 @@ public partial class MtgContext : DbContext
 
     public virtual DbSet<Models.Type> Types { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=mtg;User Id=sa;Password=Password.01;TrustServerCertificate=true");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=mtg;User Id=sa;Password=Password.01;TrustServerCertificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Howest.MagicCards.DAL/Repositories/sqlCardRepository.cs
@@ -0,0 +1,26 @@
+﻿using Howest.MagicCards.DAL.DBContext;
+using Howest.MagicCards.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Repositories;
+
+public class SqlCardRepository : ICardRepository
+{
+    private readonly MtgContext _db;
+
+    public SqlCardRepository(MtgContext myMtgContext)
+    {
+        _db = myMtgContext;
+    }
+
+    public IQueryable<Card> getAllCards()
+    {
+        IQueryable<Card> AllCards = _db.Cards.Select(c => c);
+        return AllCards;
+    }
+}

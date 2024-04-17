34d05715728092e9fc826cb522ac0495ea6f8237
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-17 10:18:03 +0200

    created Card and Type model

---
+++ Howest.MagicCards.DAL/Models/Card.cs
@@ -0,0 +1,28 @@
+﻿namespace Howest.MagicCards.DAL.Models;
+
+public class Card
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? ManaCost { get; set; }
+    public string ConvertedManaCost { get; }
+    public string Type { get; set; }
+    public string Rarity_code { get; set; }
+    public string? Text { get; set; }
+    public string? Flavor { get; set; }
+    public int Artist_id { get; set; }
+    public string Number { get; set; }
+    public string? Power { get; set; }
+    public string? Toughness { get; set; }
+    public string Layout { get; set; }
+    public int? multiverseId { get; set; }
+    public string? Original_image_url { get; set; }
+    public string Image { get; set; }
+    public string? Original_text { get; set; }
+    public string? Original_type { get; set; }
+    public string Mtg_id { get; set; }
+    public string? Variations { get; set; }
+    public DateOnly? Created_at { get; set; }
+    public DateOnly? Updated_at { get; set; }
+
+}
---
+++ Howest.MagicCards.DAL/Models/Type.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howest.MagicCards.DAL.Models
+{
+    public class Type
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public DateOnly? Created_at { get; set; }
+        public DateOnly? Updated_at { get; set; }
+    }
+}

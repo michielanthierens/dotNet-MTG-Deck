bb905f577013421651d627a1e936e2d93e345b4f
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-18 16:05:28 +0200

    detailDTO first version with mapping done

--- Howest.MagicCards.Shared/DTO/CardReadDetailDTO.cs
+++ Howest.MagicCards.Shared/DTO/CardReadDetailDTO.cs
@@ -1,17 +1,16 @@
-﻿using Howest.MagicCards.DAL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Howest.MagicCards.Shared.DTO;
+﻿namespace Howest.MagicCards.Shared.DTO;
 
 public class CardReadDetailDTO
 {
+    public string Number { get; set; }
+
     public string CardName { get; set; }
 
+    public string Power { get; set; }
+
     public string ConvertedManaCost { get; set; }
+
+    public string Toughness { get; set; }
 
     public string Type { get; set; }
 
@@ -25,17 +24,13 @@ public class CardReadDetailDTO
 
     public string Flavor { get; set; }
 
-    public string Number { get; set; }
-
-    public string Power { get; set; }
-
-    public string Toughness { get; set; }
-
     public string OriginalImageUrl { get; set; }
+
+    public string Image { get; set; }
 
     public string OriginalType { get; set; }
 
-    public virtual ICollection<CardColor> CardColors { get; set; } = new List<CardColor>();
+    public virtual IEnumerable<string> CardColors { get; set; } = new List<string>();
 
-    public virtual ICollection<CardType> CardTypes { get; set; } = new List<CardType>();
+    public virtual ICollection<string> CardTypes { get; set; } = new List<string>();
 }
--- Howest.MagicCards.Shared/Mappings/CardsProfile.cs
+++ Howest.MagicCards.Shared/Mappings/CardsProfile.cs
@@ -13,6 +13,8 @@ public class CardsProfile : Profile
             .ForMember(dto => dto.CardName, opt => opt.MapFrom(p => p.Name))
             .ForMember(dto => dto.Rarity, opt => opt.MapFrom(p => p.RarityCodeNavigation.Name))
             .ForMember(dto => dto.Set, opt => opt.MapFrom(p => p.SetCodeNavigation.Name))
-            .ForMember(dto => dto.ArtistName, opt => opt.MapFrom(p => p.Artist.FullName));
+            .ForMember(dto => dto.ArtistName, opt => opt.MapFrom(p => p.Artist.FullName))
+            .ForMember(dto => dto.CardColors, opt => opt.MapFrom(p => p.CardColors.Select(cc => cc.Color.Name)))
+            .ForMember(dto => dto.CardTypes, opt => opt.MapFrom(p => p.CardTypes.Select(ct => ct.Type.Name)));
     }
 }

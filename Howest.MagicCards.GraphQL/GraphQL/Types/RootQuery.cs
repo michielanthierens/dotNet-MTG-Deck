7ae5c3c0ef76d29462898ab66f9c9e0bc3aa3304
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-22 11:00:02 +0200

    added all field Elements to rootQuery

--- Howest.MagicCards.GraphQL/GraphQL/Types/RootQuery.cs
+++ Howest.MagicCards.GraphQL/GraphQL/Types/RootQuery.cs
@@ -1,4 +1,5 @@
-﻿using GraphQL.Types;
+﻿using GraphQL;
+using GraphQL.Types;
 using Howest.MagicCards.DAL.Repositories;
 
 namespace Howest.MagicCards.GraphQL.GraphQL.Types
@@ -8,11 +9,31 @@ namespace Howest.MagicCards.GraphQL.GraphQL.Types
         public RootQuery(ICardRepository cardRepo)
         {
             Field<ListGraphType<CardType>>(
-                "cards",
+                "All Cards",
                 Description = "Get all cards",
                 resolve: context =>
                 {
                     return cardRepo.getAllCards();
+                });
+
+            Field<ListGraphType<CardType>>(
+                "All Artists",
+                Description = "Get all artists",
+                resolve: context =>
+                {
+                    return cardRepo.getAllArtists();
+                });
+
+            Field<ListGraphType<CardType>>(
+                "Artists",
+                Description = "Get artist",
+                arguments: new QueryArguments
+                {
+                    new QueryArgument<IntGraphType> { Name = "artistId"}
+                },
+                resolve: context =>
+                {
+                    return cardRepo.GetArtist(context.GetArgument<int>("artistId"));
                 });
         }
     }

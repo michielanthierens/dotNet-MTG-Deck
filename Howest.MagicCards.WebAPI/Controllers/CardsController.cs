9a5567ecb95a9c5e49bdb7103d78d7858cbfd092
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-22 09:41:29 +0200

    prepare for merge

--- Howest.MagicCards.WebAPI/Controllers/CardsController.cs
+++ Howest.MagicCards.WebAPI/Controllers/CardsController.cs
@@ -45,7 +45,7 @@ public class CardsController : ControllerBase
             if (!_cache.TryGetValue(_key, out IEnumerable<CardReadDetailDTO> cachedResult))
             {
                 cachedResult = await _cardRepo.getAllCards()
-                            .ToFilteredList()
+                            // .ToFilteredList()
                             .ToPagedList(filter.PageNumber, filter.PageSize)
                             .ProjectTo<CardReadDetailDTO>(_mapper.ConfigurationProvider)
                             .ToListAsync();

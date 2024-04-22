421c168d997e18f6a2a505604e592023d8c745e1
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-22 11:33:16 +0200

    added filteredListFor graphql

--- Howest.MagicCards.GraphQL/GraphQL/Types/RootQuery.cs
+++ Howest.MagicCards.GraphQL/GraphQL/Types/RootQuery.cs
@@ -1,24 +1,28 @@
 ﻿using GraphQL;
 using GraphQL.Types;
 using Howest.MagicCards.DAL.Repositories;
+using Howest.MagicCards.Shared.Extensions;
 
 namespace Howest.MagicCards.GraphQL.GraphQL.Types
 {
     public class RootQuery : ObjectGraphType
     {
-        public RootQuery(ICardRepository cardRepo)
+        public RootQuery(ICardRepository cardRepo, IArtistRepository artistRepo)
         {
             Field<ListGraphType<CardType>>(
                 "All Cards",
                 Description = "Get all cards",
                 arguments: new QueryArguments
                 {
-                    new QueryArgument<IntGraphType> { Name = "power" },
-                    new QueryArgument<IntGraphType> { Name = "thoughness" }
+                    new QueryArgument<IntGraphType> { Name = "power", DefaultValue = null },
+                    new QueryArgument<IntGraphType> { Name = "thoughness", DefaultValue = null }
                 },
                 resolve: context =>
                 {
-                    return cardRepo.getAllCards();
+                    int? power = context.GetArgument<int?>("power");
+                    int? toughness = context.GetArgument<int?>("toughness");
+                    //todo return filtered list
+                    return cardRepo.getAllCards().ToFilteredListGraphQL(power, toughness);
                 });
 
             Field<ListGraphType<CardType>>(
@@ -26,11 +30,12 @@ namespace Howest.MagicCards.GraphQL.GraphQL.Types
                 Description = "Get all artists",
                 arguments: new QueryArguments
                 {
-                    new QueryArgument<IntGraphType> { Name = "limit" },
+                    new QueryArgument<IntGraphType> { Name = "limit", DefaultValue = 500 },
                 },
                 resolve: context =>
                 {
-                    return cardRepo.getAllArtists();
+                    int limit = context.GetArgument<int>("limit");
+                    return artistRepo.GetAllArtists().Take(limit);
                 });
 
             Field<ListGraphType<CardType>>(
@@ -42,7 +47,7 @@ namespace Howest.MagicCards.GraphQL.GraphQL.Types
                 },
                 resolve: context =>
                 {
-                    return cardRepo.GetArtist(context.GetArgument<int>("artistId"));
+                    return artistRepo.GetArtistById(context.GetArgument<int>("artistId"));
                 });
         }
     }

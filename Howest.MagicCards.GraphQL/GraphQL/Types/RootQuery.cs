961f56909568ddab88e70a0f008aee23682c37be
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-22 10:54:08 +0200

    added RootQuery

---
+++ Howest.MagicCards.GraphQL/GraphQL/Types/RootQuery.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using Howest.MagicCards.DAL.Repositories;
+
+namespace Howest.MagicCards.GraphQL.GraphQL.Types
+{
+    public class RootQuery : ObjectGraphType
+    {
+        public RootQuery(ICardRepository cardRepo)
+        {
+            Field<ListGraphType<CardType>>(
+                "cards",
+                Description = "Get all cards",
+                resolve: context =>
+                {
+                    return cardRepo.getAllCards();
+                });
+        }
+    }
+}

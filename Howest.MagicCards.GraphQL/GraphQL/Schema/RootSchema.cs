c08a9dd3f59ad2df179cf3a7ee55dfc6efd1c000
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-04-22 10:23:09 +0200

    added default code form slides, packages throwing errors

---
+++ Howest.MagicCards.GraphQL/GraphQL/Schema/RootSchema.cs
@@ -0,0 +1,11 @@
+﻿namespace Howest.MagicCards.GraphQL.Schema
+{
+    public class RootSchema: Schema
+    {
+        public RootSchema(IServiceProvider provider) : base(provider)
+        {
+            Query = provider.GetRequiredService<RootQuery>();
+            Mutation = provider.GetRequiredService<RootMutation>();
+        }
+    }
+}
--- Howest.MagicCards.GraphQL/Program.cs
+++ Howest.MagicCards.GraphQL/Program.cs
@@ -1,4 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using Howest.MagicCards.DAL.DBContext;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using Howest.MagicCards.DAL.Repositories;
+using Howest.MagicCards.WebAPI.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Howest.MagicCards.GraphQL.Schema;
+using GraphQL;
+
+var (builder, services, conf) = WebApplication.CreateBuilder(args);
+
+services.AddDbContext<MtgContext>(options => options.UseSqlServer(conf.GetConnectionString("mtgDB")));
+services.AddScoped<ICardRepository, SqlCardRepository>();
+// add other repo's 
+
+services.AddScoped<RootSchema>();
+
+services.AddGraphQL()
+    .AddGraphTypes(typeof(RootSchema), ServiceLifetime.Scoped)
+    .AddDataLoader()
+    .AddSystemTextJson();
+
+
 var app = builder.Build();
+
+app.UseGraphQL<RootSchema>();
+
+app.UseGraphQLPlayground(
+    "/ui/playground",
+    new PlaygroundOptions()
+    {
+        EditorTheme = EditorTheme.Light
+    });
 
 app.Run();

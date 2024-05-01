45f27d43254ccff96df0d3fe09eb7ad6c89c8357
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-05-01 12:04:08 +0200

    added apiversioning + addswaggergen to program.cs

--- Howest.MagicCards.WebAPI/Program.cs
+++ Howest.MagicCards.WebAPI/Program.cs
@@ -2,10 +2,12 @@
 using Howest.MagicCards.DAL.DBContext;
 using Howest.MagicCards.DAL.Repositories;
 using Howest.MagicCards.Shared.Mappings;
-using Howest.MagicCards.WebAPI.Extensions;
 using Howest.MagicCards.WebAPI.BehaviourConf;
+using Howest.MagicCards.WebAPI.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var (builder, services, conf) = WebApplication.CreateBuilder(args);
 
@@ -14,7 +16,21 @@ var (builder, services, conf) = WebApplication.CreateBuilder(args);
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddMemoryCache();
-services.AddSwaggerGen();
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1.1", new OpenApiInfo
+    {
+        Title = "Books API version 1.1",
+        Version = "v1.1",
+        Description = "API to return cards"
+    });
+    c.SwaggerDoc("v1.5", new OpenApiInfo
+    {
+        Title = "Books API version 1.5",
+        Version = "v1.5",
+        Description = "API to return cards"
+    });
+});
 
 services.Configure<ApiBehaviourConf>(conf.GetSection("ApiSettings"));
 
@@ -33,8 +49,10 @@ services.AddApiVersioning(o =>
     o.ReportApiVersions = true;
     o.AssumeDefaultVersionWhenUnspecified = true;
     o.DefaultApiVersion = new ApiVersion(1, 1);
+    o.ApiVersionReader = ApiVersionReader.Combine(
+                                               new QueryStringApiVersionReader("version"),
+                                               new HeaderApiVersionReader("api-version"));
 });
-
 
 
 var app = builder.Build();

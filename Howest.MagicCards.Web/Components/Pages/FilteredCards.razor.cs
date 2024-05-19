ccf39115f93c898bb9090c74eb24f9879aaca44d
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-05-19 18:43:24 +0200

    Pausing. Working on connection between webapi and blazor.

---
+++ Howest.MagicCards.Web/Components/Pages/FilteredCards.razor.cs
@@ -0,0 +1,38 @@
+﻿using Howest.MagicCards.DAL.Models;
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Howest.MagicCards.Web.Components.Pages
+{
+    public class FilteredCards
+    {
+        public FilteredCards()
+        {
+
+            JsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient("WebAPI");
+            HttpResponseMessage response = await httpClient.GetAsync($"Cards?" +
+                                                                    $"Name={Name}&" +
+                                                                    $"SetId={SetId}&" +
+                                                                    $"ArtistName={ArtistName}&" +
+                                                                    $"RarityCode={RarityCode}&" +
+                                                                    $"Type={Card_Type}&" +
+                                                                    $"Text={CardText}&" +
+                                                                    $"MaxPageSize={MaxPageSize}&" +
+                                                                    $"PageNumber={PageNumber}&" +
+                                                                    $"PageSize={PageNumber}&" +
+                                                                    $"sort=asc");
+
+
+        }
+
+    }
+}

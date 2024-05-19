72abfe4fba3f9db0939207f116046b99d789e8dd
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-05-19 17:37:13 +0200

    preparing for first webcall on blazer. Changed cardreadDTO to see the imageurl in blazor.

--- Howest.MagicCards.Shared/DTO/CardReadDTO.cs
+++ Howest.MagicCards.Shared/DTO/CardReadDTO.cs
@@ -12,19 +12,8 @@ namespace Howest.MagicCards.Shared.DTO
 
         public string Name { get; set; }
 
-        // todo remove these after testing
-        public string Type { get; set; }
-
-        public string Rarity { get; set; }
-
-        public string Set { get; set; }
-
-        public string Text { get; set; }
-
-        public string ArtistName { get; set; }
-
         // use image for blazor application
-        // public string OriginalImageUrl { get; set; }
+        public string OriginalImageUrl { get; set; }
 
     }
 }

86a2fd8e10dc52a252ef73a5d7e75c30cb618254
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-05-20 12:04:47 +0200

    added code that fetches the data from webapi. cards still not shown on screen.

--- Howest.MagicCards.Web/Components/Pages/FilteredCards.razor.cs
+++ Howest.MagicCards.Web/Components/Pages/FilteredCards.razor.cs
@@ -1,15 +1,30 @@
 ﻿using Howest.MagicCards.DAL.Models;
+using Howest.MagicCards.Shared.DTO;
+using Howest.MagicCards.WebAPI.Wrappers;
 using Microsoft.AspNetCore.Http.Json;
 using System.Text.Json;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Howest.MagicCards.Web.Components.Pages
 {
-    public class FilteredCards
+    public partial class FilteredCards
     {
+        public IEnumerable<CardReadDTO> filteredCards { get; set; }
+        public string message { get; set; }
+        public string Name { get; set; }
+        public string SetId { get; set; }
+        public string ArtistName { get; set; }
+        public string RarityCode { get; set; }
+        public string Card_Type { get; set; }
+        public string Card_Text { get; set; }
+        public string MaxPageSize { get; set; }
+        public string PageNumber { get; set; }
+        public string PageSize { get; set; }
+        public string Sort { get; set; }
+        public JsonSerializerOptions JsonOptions { get; }
+
         public FilteredCards()
         {
-
             JsonOptions = new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true,
@@ -25,14 +40,27 @@ namespace Howest.MagicCards.Web.Components.Pages
                                                                     $"ArtistName={ArtistName}&" +
                                                                     $"RarityCode={RarityCode}&" +
                                                                     $"Type={Card_Type}&" +
-                                                                    $"Text={CardText}&" +
+                                                                    $"Text={Card_Text}&" +
                                                                     $"MaxPageSize={MaxPageSize}&" +
                                                                     $"PageNumber={PageNumber}&" +
-                                                                    $"PageSize={PageNumber}&" +
-                                                                    $"sort=asc");
+                                                                    $"PageSize={PageSize}&" +
+                                                                    $"sort={Sort}");
 
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                PagedResponse<IEnumerable<CardReadDTO>> result = JsonSerializer.Deserialize<PagedResponse<IEnumerable<CardReadDTO>>>(apiResponse, JsonOptions);
+
+                filteredCards = result.Data;
+            }
+            else
+            {
+                filteredCards = new List<CardReadDTO>();
+                message = $"Error: {response.ReasonPhrase}";
+
+
+            }
 
         }
-
     }
 }

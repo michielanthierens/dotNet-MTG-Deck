4ed7347325b93c5fb646e5b03e8cf94372313f4a
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-05-24 16:15:50 +0200

    removing comments

--- Howest.MagicCards.WebAPI/Program.cs
+++ Howest.MagicCards.WebAPI/Program.cs
@@ -10,8 +10,6 @@ using Microsoft.OpenApi.Models;
 
 var (builder, services, conf) = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 services.AddMemoryCache();
@@ -56,21 +54,13 @@ o.ApiVersionReader = ApiVersionReader.Combine(
 builder.Services.AddVersionedApiExplorer(
     options =>
     {
-        // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
-        // note: the specified format code will format the version as "'v'major[.minor][-status]"
         options.GroupNameFormat = "'v'VVV";
-
-        // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
-        // can also be used to control the format of the API version in route templates
         options.SubstituteApiVersionInUrl = true;
     }
 );
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();

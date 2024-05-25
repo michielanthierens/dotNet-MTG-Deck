7f4e4d02a89bce6cf8e99bccd7227fde966ce4df
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-05-25 16:09:48 +0200

    added partial class for home

---
+++ Howest.MagicCards.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+using Howest.MagicCards.Shared.DTO;
+using Howest.MagicCards.WebAPI.Wrappers;
+
+namespace Howest.MagicCards.Web.Components.Pages;
+
+public partial class Home
+{
+    private IEnumerable<DeckReadDTO> DeckCards { get; set; }
+    private JsonSerializerOptions JsonOptions { get; }
+    private string _message;
+
+    public Home()
+    {
+        JsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+    }
+
+    private async Task GetDeck()
+    {
+        HttpClient httpClient = httpClientFactory.CreateClient("MinimalAPI");
+
+        HttpResponseMessage response = await httpClient.GetAsync("");
+
+        string apiResponse = await response.Content.ReadAsStringAsync();
+
+        if (response.IsSuccessStatusCode)
+        {
+            Response<IEnumerable<DeckReadDTO>> apiResponseObj = JsonSerializer.Deserialize<Response<IEnumerable<DeckReadDTO>>>(apiResponse, JsonOptions);
+            if (apiResponseObj is { Succeeded: true })
+            {
+                DeckCards = apiResponseObj.Data ?? new List<DeckReadDTO>();
+                _message = null;
+            }
+            else
+            {
+                _message = apiResponseObj?.Message;
+            }
+        }
+        else
+        {
+            _message = $"Error: {response.ReasonPhrase}";
+        }
+    }
+}

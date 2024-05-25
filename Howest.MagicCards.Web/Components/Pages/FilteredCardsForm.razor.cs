a06736ddf9598228f8c860259efd340b8b8d4121
michiel.anthierens <michiel.anthierens@student.howest.be>
2024-05-25 14:48:54 +0200

    made a partial class for the form

---
+++ Howest.MagicCards.Web/Components/Pages/FilteredCardsForm.razor.cs
@@ -0,0 +1,57 @@
+﻿using Howest.MagicCards.Shared.DTO;
+using Howest.MagicCards.Shared.Filters;
+using Microsoft.AspNetCore.Components;
+using System.Text.Json;
+
+namespace Howest.MagicCards.Web.Components.Pages
+{
+    public partial class FilteredCardsForm
+    {
+        [Parameter]
+        public BlazorFilter BlazorFilter { get; set; }
+
+        [Parameter]
+        public string Message { get; set; }
+
+        [Parameter]
+        public EventCallback LoadFilteredCards { get; set; }
+
+        private IEnumerable<RarityDTO> rarities { get; set; } = new List<RarityDTO>();
+        private JsonSerializerOptions JsonOptions { get; }
+
+
+        public FilteredCardsForm()
+        {
+            JsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            GetRarities();
+
+        }
+
+        private async void GetRarities()
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient("WebAPI");
+            httpClient.DefaultRequestHeaders.Add("api-version", "1.5");
+            // double check
+            HttpResponseMessage response = await httpClient.GetAsync("Cards/rarities");
+
+            string apiResponse = await response.Content.ReadAsStringAsync();
+
+            if (response.IsSuccessStatusCode)
+            {
+                rarities = JsonSerializer.Deserialize<IEnumerable<RarityDTO>>(apiResponse, JsonOptions);
+            }
+            else
+            {
+                rarities = new List<RarityDTO>();
+                Message = $"Error: {response.ReasonPhrase}";
+            }
+        }
+    }
+}
